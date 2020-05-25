907b17b4bdb6b81ec33af69bbfbd2f07d66fdf0a
choiiis <haemin0128@naver.com>
2020-05-25 23:30:22 +0900

    get frames from baby_vid.mp4

--- getVidFrame.py
+++ getVidFrame.py
@@ -1,22 +1,30 @@
 import cv2
 
-# 영상의 의미지를 연속적으로 캡쳐할 수 있게 하는 class
-vidcap = cv2.VideoCapture("C:\\Users\\Haemin\\Documents\\_choiiis\\balanchew\\baby_vid.mp4")
+# Create VideoCapture object; open video file
+vid_input = cv2.VideoCapture("baby_vid.mp4")
+
+# If you want to use your web-camera,
+# cv2.VideoCapture(0)
+
 count = 0
 print("start")
-while (vidcap.isOpened()):
-    # read()는 grab()와 retrieve() 두 함수를 한 함수로 불러옴
-    # 두 함수를 동시에 불러오는 이유는 프레임이 존재하지 않을 때
-    # grab() 함수를 이용하여 return false 혹은 NULL 값을 넘겨 주기 때문
-    ret, image = vidcap.read()
 
-    # 캡쳐된 이미지를 저장하는 함수
+# check video file open successfully
+while (vid_input.isOpened()):
+    # Get frame from video
+    # get success : ret = True / fail : ret= False
+    ret, frame = vid_input.read()
+
+    # fail to get frame
     if not ret:
         break
-    else:
-        cv2.imwrite("C:\\Users\\Haemin\\Documents\\_choiiis\\balanchew\\frame%d.jpg" % count, image)
 
-    print('Saved frame%d.jpg' % count)
+    # succeed
+    else:
+        # save the frame
+        cv2.imwrite("C:\\Users\\Haemin\\Documents\\GitHub\\balanchew\\video_frame\\frame%d.jpg" % count, frame)
+
+    print('frame%d.jpg saved!' % count)
     count += 1
 
-vidcap.release()
+vid_input.release()
