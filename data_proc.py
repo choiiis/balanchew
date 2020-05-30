60acf2d0177c4c0caa999a74154e20a555603ef4
choiiis <haemin0128@naver.com>
2020-05-30 22:33:42 +0900

    analyze left/right & add dataset

--- data_proc.py
+++ data_proc.py
@@ -3,57 +3,56 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 right_dict_list = []
+left_dict_list = []
 
-with open("right.json", "r") as f:
+with open("left_4(sis).json", "r") as f:
     lines = f.readlines()
-    count = 0
+    lcount = 0
+    for line in lines:
+        left_data = json.loads(line)
+        # count : line num
+        print(lcount)
+        lcount += 1
+        left_dict_list.append(left_data)
+
+with open("right_4(sis).json", "r") as f:
+    lines = f.readlines()
+    rcount = 0
     for line in lines:
         right_data = json.loads(line)
         # count : line num
-        print(count)
-        count += 1
+        print(rcount)
+        rcount += 1
         right_dict_list.append(right_data)
 
 
-matrix = [[0 for col in range(68)] for row in range(count)]
+landmark_info = []
 
-for i in range(0,count):
+for i in range(0,lcount):
     for j in range(0, 68):
-        matrix[i][j] = right_dict_list[i][str(j)][0]
-        print(j, " ", right_dict_list[i][str(j)][0])
+        landmark_info.append([j, left_dict_list[i][str(j)][0], 'x', 'left'])
+        landmark_info.append([j, left_dict_list[i][str(j)][1], 'y', 'left'])
 
-x = np.arange(0, 68)
+for i in range(0,rcount):
+    for j in range(0, 68):
+        landmark_info.append([j, right_dict_list[i][str(j)][0], 'x', 'right'])
+        landmark_info.append([j, right_dict_list[i][str(j)][1], 'y', 'right'])
 
-for i in range(0,count):
-    plt.scatter(x, matrix[i], 1, label=str(i))
+landmark_df = pd.DataFrame(landmark_info, columns=['id', 'value', 'XY', 'LR'])
+print(landmark_df)
+landmark_df_x = landmark_df.loc[landmark_df["XY"] == "x"]
+landmark_df_y = landmark_df.loc[landmark_df["XY"] == "y"]
+
+plt.figure(figsize=(8,6))
+sns.boxplot(x="id", y="value", hue="LR", data=landmark_df_x)
+# sns.swarmplot(x="id", y="value", hue="LR", data=landmark_df_x, size = 1)
 
 plt.xlabel('face landmarks')
 plt.ylabel('coordinates')
 
-plt.title('Landmarks analysis')
+plt.title('Landmarks analysis - Y')
 plt.legend(loc='upper right')
 plt.show()
-
-
-
-
-
-#     lines = f.readlines()
-#     jsonDF = pd.DataFrame(json.loads(line) for line in lines)
-#     count = 0;
-#     for line in lines:
-#         count+= 1
-#
-# df = pd.DataFrame.from_records(jsonDF, columns=['1'])
-#
-# plt.title('left 1')
-# df['1'].plot(label='1')
-# plt.show(block=True)
-
-
-
-
-
-# with open("right.json", "w") as json_file:
